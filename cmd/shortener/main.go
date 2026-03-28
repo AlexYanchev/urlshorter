8bc8b35264c280c3b0eb3bc07f502e9818c675d9
Алексей Янчев <yanchevalexey@yandex.ru>
2026-03-28 18:31:39 +0300

    ink 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/AlexYanchev/urlshorter/internal/handler"
+)
+
+func main() {
+	h := handler.New()
+
+	http.HandleFunc("/", h.CreateShortURL)
+	http.HandleFunc("/{id}", h.RedirectURL)
+
+	log.Println("Server starting on localhost:8080")
+	
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
